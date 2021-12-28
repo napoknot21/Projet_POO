82dca54e2bd25e4c71f24e63d1662f75016e1a10
cmartinavila <charly.martin-avila@etu.u-paris.fr>
2021-12-28 19:33:49 +0100

    Initial commit

---
+++ src/WelcomeWindow.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class WelcomeWindow extends JFrame {
+
+    public WelcomeWindow() {
+        this.setSize(1920,1080);
+        this.setTitle("Les Colons de Catan!");
+        ImageIcon fond = new ImageIcon("./images/catan_intro_2.jpg");
+        JLabel background = new JLabel("",fond,JLabel.CENTER);
+        background.setBounds(0,0,1920,1080);
+        this.add(background);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main (String[] args) {
+        WelcomeWindow w = new WelcomeWindow();
+    }
+    
+    
+}
