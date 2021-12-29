00c9a628428fce8bd2bfd23f980d9fc8e4d7118a
cmartinavila <charly.martin-avila@etu.u-paris.fr>
2021-12-29 15:36:37 +0100

    CarteDeveleppement.java mise a jour

--- src/CarteDeveleppement.java
+++ src/CarteDeveleppement.java
@@ -1,2 +1,19 @@
-public class CarteDeveleppement {
+import java.awt.*;
+
+public class CarteDeveleppement extends Carte {
+
+    private Color color;
+    /*
+    On peut avoir les couleurs suivantes:
+    -> Bleu : Knight
+    -> Vert : Lieux (Bibliothèque, Librairie, etc)
+    -> Jaune : Point de victoire
+    */
+
+    public CarteDeveleppement (String titre, String carteType, String description,Color color) {
+        super(titre,carteType,description);
+        this.color = color;
+    }
+
+
 }
