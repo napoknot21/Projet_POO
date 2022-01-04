8fc79e9d832b1b25058a20d4fc98e84a7d25ce6d
napoknot21 <napoknot21@protonmail.com>
2022-01-04 21:39:29 +0100

    Amelioration de l'interface

--- src/ig/Game.java
+++ src/ig/Game.java
@@ -1,12 +1,17 @@
 package ig;
 
+import java.awt.*;
+
 public class Game {
 
     private WelcomeWindow ww;
 
-
     public Game () {
-        this.ww = new WelcomeWindow();
+        EventQueue.invokeLater(() -> {
+            this.ww = new WelcomeWindow();
+            this.ww.setVisible(true);
+        });
+
     }
 
 }
