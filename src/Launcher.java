b0f7486a50d24f1758f42fed511493eff802b04d
napoknot21 <napoknot21@protonmail.com>
2022-01-08 15:57:40 +0100

    Board.java settings

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,17 +1,18 @@
 import game.Game;
+import game.GameLoop;
+import ig.WelcomeWindow;
+import java.awt.*;
+import java.util.Scanner;
 
 public class Launcher {
 
+    private WelcomeWindow ww;
+
     public Launcher() {
-        Game g = new Game();
+            new Thread((new GameLoop(new Game()))).start();
     }
 
     public static void main (String[] args) {
-        new Launcher();
-        //new Launcher();
-         //new Thread(new GameLoop(new Game())).start();
-         //new Launcher();
-        /*
         Scanner sc = new Scanner(System.in);
         System.out.println("Vous voulez jouer au Colon de Catan? [oui/non] ");
         String rep = sc.nextLine();
@@ -23,7 +24,8 @@ public class Launcher {
                 rep = sc.nextLine();
                 if (rep.equals("ig") || rep.equals("interface graphique")) {
                     System.out.println("D'accord!");
-                    Game g = new Game();
+                    new Launcher();
+                    state = false;
                 } else if (rep.equals("t") || rep.equals("terminal")) {
                     System.out.println("D'accord!");
                     System.out.println("Désolé :( cette option est encore en construction");
@@ -34,10 +36,14 @@ public class Launcher {
                 System.exit(0);
             } else {
                 System.out.println("Essayez de nouveau");
-                state = false;
             }
         }
-*/
+        System.out.println("Merci de avoir lancé la version beta");
+        System.out.println("Le jeu aura de grands changements");
+        System.out.println("Auteurs:\n" +
+                "   Martin CHAMPION\n" +
+                "   Charly MARTIN AVILA"
+        );
     }
 
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -1,19 +1,34 @@
 package game;
 
+import board.Board;
 import ig.WelcomeWindow;
 
 import java.awt.*;
+import java.lang.management.PlatformLoggingMXBean;
+import java.util.ArrayList;
 
 public class Game {
 
+    private Board board;
+    private ArrayList<Joueur> players;
     private WelcomeWindow ww;
 
-    public Game () {
+    public Game (/*ArrayList<Joueur> tousLesJoueurs*/) {
         EventQueue.invokeLater(() -> {
             this.ww = new WelcomeWindow();
             this.ww.setVisible(true);
+
         });
+        /*
+        if (tousLesJoueurs.size() <3 || tousLesJoueurs.size() >4) {
+            throw new IllegalArgumentException("Le jeu doit être joué pour au moins 3 personnes et au max 4");
+        }
+
+
+         */
 
     }
+
+
 
 }
