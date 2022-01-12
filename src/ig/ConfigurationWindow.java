0ae1dd3c2df1c1a8b1e83fa596b5f54edb6525cb
napoknot21 <napoknot21@protonmail.com>
2022-01-12 07:30:03 +0100

    UI settings #2

--- src/ig/ConfigurationWindow.java
+++ src/ig/ConfigurationWindow.java
@@ -6,13 +6,16 @@ import java.awt.*;
 public class ConfigurationWindow extends JFrame {
 
 
-
+    private WelcomeWindow ww;
+    private GameWindow gw;
     private JLabel background;
     public boolean status = false;
     private ImageIcon img;
 
 
-    public ConfigurationWindow () {
+    public ConfigurationWindow (WelcomeWindow ww) {
+
+        this.ww = ww;
 
         this.setTitle("Paramètres");
         this.setSize(500, 500);
@@ -20,11 +23,45 @@ public class ConfigurationWindow extends JFrame {
         this.setVisible(status);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
+
+
+        JPanel content = (JPanel) this.getContentPane();
+
+        JPanel player1 = new JPanel();
+        JPanel player2 = new JPanel();
+        JPanel player3 = new JPanel();
+        JPanel player4 = new JPanel();
+
+        JLabel name = new JLabel("Rentrez le nom du joueur");
+        player1.add(name);
+        player2.add(name);
+        player3.add(name);
+        player4.add(name);
+        String name_player = name.getText();
+
+
+        this.add(player1,BorderLayout.NORTH);
+        this.add(player2,BorderLayout.EAST);
+        this.add(player3,BorderLayout.CENTER);
+        this.add(player4,BorderLayout.SOUTH);
+
+
+
+
+
+
         JPanel bouton = new JPanel();
         bouton.setLayout(new FlowLayout(FlowLayout.CENTER));
         this.add(bouton,BorderLayout.SOUTH);
+        JButton play = new UIButton("Jouer !!!");
+        bouton.add(play);
+        play.addActionListener(event -> {
+            this.ww.setVisible(false);
+            this.setStatus(false);
+            new GameWindow().setVisible(true);
+            System.out.println("Jouer !!!");
+        });
 
-        bouton.add(new UIButton("Jouer !!!"));
 
 
     }
--- src/ig/GameWindow.java
+++ src/ig/GameWindow.java
@@ -1,9 +1,30 @@
 package ig;
 
-public class GameWindow {
+import board.Board;
+import game.Game;
 
+import javax.swing.*;
+import java.util.ArrayList;
 
+public class GameWindow extends JFrame {
 
+    public boolean status = false;
+    private ImageIcon fond;
+    private JLabel background;
+    private Game game;
+    private Board board;
+
+    public GameWindow () {
+        this.fond = new ImageIcon("./images/catan_intro_2.jpg");
+
+        this.setTitle("Colons de Catan");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(this.fond.getIconWidth(),this.fond.getIconHeight());
+        this.setLocationRelativeTo(null);
+        this.background = new JLabel("",this.fond,JLabel.CENTER);
+        this.background.setBounds(0,0,this.fond.getIconWidth(),this.fond.getIconHeight());
+        this.add(this.background);
+    }
 
 
 }
